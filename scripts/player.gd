2781a98f37c3ec88ce68cfcba74283fecd8beb3b
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-02-20 23:24:59 +0500

    the old (not epstien) project files, from Swiveling Escape, 2021
    https://alimad.itch.io/swiveling-escape

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,13 +3,12 @@ extends RigidBody2D
 @export var torque_power := 110000.0
 @export var max_angular_velocity := 24.0
 
-@export var ground_force := 1400.0
-@export var air_force := 200.0
+@export var jump_force := 800.0
+@export var help_force := 400.0
 
 @export var jump_impulse := 9000.0
 
 @onready var ray := $GroundRay
-
 
 func _physics_process(delta):
 	handle_spin()
@@ -22,15 +21,11 @@ func handle_spin():
 
 	if input_dir != 0:
 		apply_torque(input_dir * torque_power)
-
-	# Small directional assist so it doesn't feel like ice
-	var force = ground_force if is_on_floor() else air_force
-	apply_force(Vector2(input_dir * force, 0))
-
+		apply_force(Vector2(input_dir * help_force, 0))
 
 func handle_jump():
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		apply_impulse(Vector2.ZERO, Vector2(0, -jump_impulse))
+	if Input.is_action_just_pressed("jump"):
+		apply_impulse(Vector2(0, -jump_force))
 
 
 func clamp_spin():
