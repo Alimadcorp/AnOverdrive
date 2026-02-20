da877ca24ddab9e03db4b752d481b2e29ca1e95e
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-02-21 00:51:13 +0500

    audio convert

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,24 +2,37 @@ extends RigidBody2D
 
 @export var torque_power := 110000.0
 @export var max_angular_velocity := 24.0
-
 @export var jump_force := 800.0
 @export var help_force := 400.0
 
-@export var jump_impulse := 9000.0
+# --- Audio Exports ---
+@export var collision_sounds: Array[AudioStream] = []
+@export var jump_sound: AudioStream
+@export var collect_sound: AudioStream
 
+@onready var collision_player := $CollisionPlayer
+@onready var action_player := $ActionPlayer
+@onready var torque_player := $TorquePlayer
 @onready var ray := $GroundRay
 
 var collected_keys: Array[Color] = []
 
+func _ready() -> void:
+	# Enable contact monitoring to detect collisions
+	contact_monitor = true
+	max_contacts_reported = 5
+	body_entered.connect(_on_body_entered)
+
 func add_key(color: Color) -> void:
 	collected_keys.append(color)
+	# Play collect sound (same player as jump is fine)
+	action_player.stream = collect_sound
+	action_player.play()
 
 func _physics_process(delta):
 	handle_spin()
 	handle_jump()
 	clamp_spin()
-
 
 func handle_spin():
 	var input_dir = Input.get_axis("left", "right")
@@ -27,20 +40,31 @@ func handle_spin():
 	if input_dir != 0:
 		apply_torque(input_dir * torque_power)
 		apply_force(Vector2(input_dir * help_force, 0))
+		
+		# Torque Sound Logic
+		if not torque_player.playing:
+			torque_player.play()
+	else:
+		# Stop sound when no input
+		if torque_player.playing:
+			torque_player.stop()
 
 func handle_jump():
 	if Input.is_action_just_pressed("jump"):
 		apply_impulse(Vector2(0, -jump_force))
+		# Jump Sound
+		action_player.stream = jump_sound
+		action_player.play()
 
+func _on_body_entered(_body: Node) -> void:
+	if collision_sounds.size() > 0:
+		# Pick one of the 5 sounds randomly
+		var random_index = randi() % collision_sounds.size()
+		collision_player.stream = collision_sounds[random_index]
+		collision_player.play()
 
 func clamp_spin():
-	angular_velocity = clamp(
-		angular_velocity,
-		-max_angular_velocity,
-		max_angular_velocity
-	)
-
+	angular_velocity = clamp(angular_velocity, -max_angular_velocity, max_angular_velocity)
 
 func is_on_floor() -> bool:
-	return true
-	# return ray.is_colliding()
+	return ray.is_colliding()
